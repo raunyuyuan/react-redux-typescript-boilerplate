89e57ca5b7bb408c7ad2b605dcd01b011d12a7ad
ruanyuyuan <765426599@qq.com>
2020-03-26 23:20:29 +0800

    Add saga and redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const MOUNT_NODE = document.getElementById('root')
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    MOUNT_NODE
+  );
+}
+
+render()
+
+// @ts-ignore
+if (module.hot) {
+  // Hot reloadable React components and translation json files
+  // modules.hot.accept does not accept dynamic dependencies,
+  // have to be constants at compile-time
+  // @ts-ignore
+  module.hot.accept(['./App'], () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE as HTMLDivElement);
+    render()
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
